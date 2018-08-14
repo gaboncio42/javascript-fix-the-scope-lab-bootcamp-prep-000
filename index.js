b38c665a5f751df094a8278059c05fc04939dfca
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-14 05:02:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction(); 
 
-function funkyFunction() {
-  var = funkyFunction;
-  return function()
+function theFunk() {
+  return funkyFunction;
 }
